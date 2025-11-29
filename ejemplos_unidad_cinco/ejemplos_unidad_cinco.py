8aef58ba612bf9acef658d426a1af836575964ad
Andrea Cea Villadeo <andreacea96@gmail.com>
2025-11-29 12:12:51 -0600

    ejemplos carpetas

---
+++ ejemplos_unidad_cinco/ejemplos_unidad_cinco.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def app():
+    st.title("🎯 Ejemplos Interactivos - Unidad II")
+    st.write("Funciones y sus propiedades")
+    st.info("Contenido de la Unidad II en desarrollo...")
